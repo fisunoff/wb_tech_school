bfff16f4f72fe8d0ea38484d53fa93a67cd4ca08
fisunoff <fisunoff@yandex.ru>
2025-08-07 10:47:30 +0300

    L0: получение данных из json-а

---
+++ Level0/order_service/model/order_test.go
@@ -0,0 +1,41 @@
+package model
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func readTestData(t *testing.T, filename string) []byte {
+	t.Helper()
+	path := filepath.Join("testdata", filename)
+	data, err := os.ReadFile(path)
+	if err != nil {
+		t.Fatalf("Не удалось прочитать тестовый файл %s: %v", filename, err)
+	}
+	return data
+}
+
+func TestSerializeOrderPositive(t *testing.T) {
+	correctData := readTestData(t, "valid_order.json")
+	_, err := SerializeOrder([]byte(correctData))
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func TestSerializeOrderInvalidSyntax(t *testing.T) {
+	correctData := readTestData(t, "invalid_syntax.json")
+	_, err := SerializeOrder([]byte(correctData))
+	if err == nil {
+		t.Error("Ожидалась ошибка, но ее нет")
+	}
+}
+
+func TestSerializeOrderInvalidType(t *testing.T) {
+	correctData := readTestData(t, "invalid_type.json")
+	_, err := SerializeOrder([]byte(correctData))
+	if err == nil {
+		t.Error("Ожидалась ошибка, но ее нет")
+	}
+}
